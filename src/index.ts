85606d7088e6717d7c54304e5f363557f6afb2d3
AlineCantalice <alinedecantalice@gmail.com>
2022-08-31 20:20:36 -0300

    add midleware

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import express from "express";
 import cors from 'cors';
 import dotenv from 'dotenv';
 import userRouter from './routers/userRouter.js';
+import handleErrorMiddleware from "./middlewares/handleErrorMiddleware.js";
 
 dotenv.config();
 
@@ -10,6 +11,7 @@ const app = express();
 app.use(cors());
 
 app.use(userRouter)
+app.use(handleErrorMiddleware)
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
